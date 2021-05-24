10abf45a431dc1a9343e650ee888830688e1dbd4
jaspreet <singhjaspreet821@gmail.com>
2021-05-25 03:20:13 +0530

    Commited Tab Modal. TODO - Implement Login/Register Tab

--- src/screens/homepage/ActionButton.js
+++ src/screens/homepage/ActionButton.js
@@ -1,12 +1,25 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
-import './ActionButton.css'
+import { Button, Tab, Tabs } from '@material-ui/core';
+import React, { useState } from 'react';
+import Modal from 'react-modal'
 
+Modal.setAppElement('#root')
 const ActionButton = () => {
 
-    return (
-        <Button variant="contained" color="default" className="action-btn">Login</Button>
-    )
-}
+    const [model, setmodel] = useState(true);
 
-export default ActionButton;
+        return (
+            <div>
+                {/* TODO - Implement Login/Register Tab using Tabs and Modals */}
+                <Button id="actionButton" variant="contained" color="default" className="action-btn" onClick={() => setmodel(true)}>Login</Button>
+                <Modal isOpen={model} onRequestClose={() => setmodel(false)}>
+                    <Tabs selectionfollowsfocus centered textColor="primary" variant="fullWidth" width="100px">
+                        <Tab label="Login"></Tab>
+                        <Tab label="Register"></Tab>
+                    </Tabs>
+                    <Button id="actionButton" variant="contained" color="primary" onClick={() => setmodel(false)}>Close</Button>
+                </Modal>
+            </div>
+        )
+    }
+
+    export default ActionButton;
